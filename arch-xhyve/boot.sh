59dfbce959cf8646926380efa027e4468daaa6f1
Giang Manh <manhg@users.noreply.github.com>
2017-11-28 11:54:02 +0900

    Add arch script

---
+++ arch-xhyve/boot.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+iso=archlinux-2017.11.01-x86_64.iso
+
+if [ ! -f $iso ]; then
+    wget http://ftp.nara.wide.ad.jp/pub/Linux/archlinux/iso/2017.11.01/archlinux-2017.11.01-x86_64.iso
+fi
+ 
+if [ ! -f archiso.img ]; then
+    echo "fixing disk"
+    dd if=/dev/zero bs=2k count=1 of=tmp.iso
+    dd if=$iso bs=2k skip=1 >> tmp.iso
+     
+    echo "mounting disk"
+    diskinfo=$(hdiutil attach tmp.iso)
+    disk=$(echo "$diskinfo" |  cut -d' ' -f1)
+    mnt=$(echo "$diskinfo" | perl -ne '/(\/Volumes.*)/ and print $1')
+    echo "mounted as $disk at $mnt"
+     
+    echo "extracting kernel"
+    ls -l "$mnt/arch/boot/x86_64"
+    cp "$mnt/arch/boot/x86_64/vmlinuz" .
+    cp "$mnt/arch/boot/x86_64/archiso.img" .
+    diskutil eject "$disk"
+    rm tmp.iso
+fi
+
+if [ ! -f disk.img ]; then
+    echo "creating hdd"
+    dd if=/dev/zero of=disk.img bs=4G count=1
+fi
+ 
+KERNEL="vmlinuz"
+INITRD="archiso.img"
+CMDLINE="console=ttyS0 archisobasedir=arch acpi=off earlyprintk=serial archisolabel=ARCH_201711"
+ 
+MEM="-m 512M"
+PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
+NET="-s 2:0,virtio-net"
+IMG_CD="-s 3,ahci-cd,$iso"
+IMG_HDD="-s 4,virtio-blk,disk.img"
+LPC_DEV="-l com1,stdio"
+ACPI="-A"
+ # http://blog.dunkelstern.de/2016/01/17/xhyve-a-quick-how-to/
+sudo xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD -f kexec,$KERNEL,$INITRD,"$CMDLINE"
+
+

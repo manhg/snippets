2c43154fa1d5d6e1c0608544786a7ac520372aa3
Giang Manh <manhg@users.noreply.github.com>
2017-12-17 23:09:04 +0900

    add ohmyzsh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 curl https://raw.githubusercontent.com/manhg/snippets/master/rc.sh > ~/.rc
 curl https://raw.githubusercontent.com/manhg/snippets/master/zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/manhg/snippets/master/bashrc > ~/.bashrc

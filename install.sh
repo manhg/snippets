3602cbdc284fdd0692bd1ac535e1a9796c1e9b16
Giang Manh <manhg@users.noreply.github.com>
2017-12-17 23:10:34 +0900

    Add micro

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 curl https://raw.githubusercontent.com/manhg/snippets/master/rc.sh > ~/.rc
 curl https://raw.githubusercontent.com/manhg/snippets/master/zshrc > ~/.zshrc
 curl https://raw.githubusercontent.com/manhg/snippets/master/bashrc > ~/.bashrc
+curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z
+
+mkdir ~/.bin
+curl https://getmic.ro | bash
+mv micro ~/.bin

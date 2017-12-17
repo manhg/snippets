fb13254424991c1516c66ad8b660110988a8a397
Giang Manh <manhg@users.noreply.github.com>
2017-12-17 23:36:04 +0900

    fix

--- debian-xhyve/boot.sh
+++ debian-xhyve/boot.sh
@@ -13,22 +13,22 @@ NET="-s 2:0,virtio-net"
 
 if [ $USE_CD ]; then
     iso=debian-9.2.1-amd64-netinst.iso
-    
+
     if [ ! -f $iso ]; then
         wget http://ftp.jaist.ac.jp/pub/Linux/debian-cd/current/amd64/iso-cd/debian-9.2.1-amd64-netinst.iso
     fi
-     
+
     if [ ! -f initrd.gz ]; then
         wget https://d-i.debian.org/daily-images/amd64/daily/netboot/debian-installer/amd64/linux
         wget https://d-i.debian.org/daily-images/amd64/daily/netboot/debian-installer/amd64/initrd.gz
     fi
-    
+
     if [ ! -f disk.img ]; then
         echo "creating hdd"
         truncate -s 8G disk.img
     fi
-    
-     
+
+
     KERNEL="linux"
     INITRD="initrd.gz"
     CMDLINE="earlyprintk=serial console=ttyS0 acpi=off "
@@ -38,7 +38,7 @@ else
     INITRD="initrd.img"
     CMDLINE="console=ttyS0,38400n8 acpi=off earlyprintk=serial root=/dev/vda1 "
 fi
-UUID=" -U 20000000-0000-0000-0000-000000000000"
+UUID=" -U 10000000-0000-0000-0000-000000000000"
 IMG_HDD="-s 4,virtio-blk,disk.img"
 LPC_DEV="-l com1,stdio"
 ACPI="-A"

91b3ede67d95b8c1384a3c197720759e66590e8b
Giang Manh <manhg@users.noreply.github.com>
2017-12-19 05:42:02 +0900

    micro shortcut look nano

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
+MY=https://raw.githubusercontent.com/manhg/snippets/master
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl https://raw.githubusercontent.com/manhg/snippets/master/.rc > ~/.rc
-curl https://raw.githubusercontent.com/manhg/snippets/master/zshrc > ~/.zshrc
-curl https://raw.githubusercontent.com/manhg/snippets/master/bashrc > ~/.bashrc
+curl $MY/.rc > ~/.rc
+curl $MY/zshrc > ~/.zshrc
+curl $MY/bashrc > ~/.bashrc
 curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z
 
 mkdir ~/.bin
 curl https://getmic.ro | bash
 mv micro ~/.bin
+mkdir -p ~/.config/micro
+curl $MY/micro_shortcuts.json ~/.config/micro/bindings.json

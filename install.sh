ffa43c3c0d8f10771311d713815c2d3e32ff7f05
Giang Manh <manhg@users.noreply.github.com>
2017-12-19 14:47:17 +0900

    check zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 MY=https://raw.githubusercontent.com/manhg/snippets/master
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl $MY/.rc > ~/.rc
-curl $MY/zshrc > ~/.zshrc
 curl $MY/bashrc > ~/.bashrc
-curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z
+
+if [ command -v zsh ]; then
+    curl $MY/zshrc > ~/.zshrc
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    curl https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z
+fi
 
 mkdir ~/.bin
 curl https://getmic.ro | bash

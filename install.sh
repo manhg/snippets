97120104fd038f2339dfdd5de0e1203881ada38b
Jan <manhg@users.noreply.github.com>
2022-06-21 12:37:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,5 +16,10 @@ if [ ! -f /usr/local/bin/dry ]; then
     sudo chmod 755 /usr/local/bin/dry
 fi
 
+apt-get install bash-completion curl git docker-compose postfix ufw psmisc fail2ban gnupg
+ufw allow 9622 
+ufw allow 22
+ufw allow 80
+ufw allow 443
 curl -L https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z.sh
 echo 'source .z.sh' >> .bashrc

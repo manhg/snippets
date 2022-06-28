9b18b16f415a12c4824f5f5f1237f7f4a635c86d
Giang Manh <manhg@users.noreply.github.com>
2022-06-28 09:38:39 +0900

    update scripts

--- install.sh
+++ install.sh
@@ -2,24 +2,19 @@ MY=https://raw.githubusercontent.com/manhg/snippets/master
 curl $MY/.rc > ~/.rc
 curl $MY/bashrc > ~/.bashrc
 echo 'source .bashrc' >> .bash_profile
-
-if [ ! -f ~/.bin/micro ]; then
-    mkdir ~/.bin
-    curl https://getmic.ro | bash
-    mv micro ~/.bin
-    mkdir -p ~/.config/micro
-    curl $MY/micro_shortcuts.json > ~/.config/micro/bindings.json
-fi
-
-if [ ! -f /usr/local/bin/dry ]; then
-    curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
-    sudo chmod 755 /usr/local/bin/dry
-fi
-
-apt-get install bash-completion curl git docker-compose postfix ufw psmisc fail2ban gnupg
-ufw allow 9622 
+apt-get update
+apt-get install bash-completion curl git docker-compose postfix ufw psmisc fail2ban gnupg logcheck rsync
+ufw allow 3389
+ufw allow 9622
 ufw allow 22
 ufw allow 80
 ufw allow 443
+ufw enable
 curl -L https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.z.sh
 echo 'source .z.sh' >> .bashrc
+
+echo '\nPort = 9622\nPasswordAuthentication no' >> /etc/ssh/sshd_config
+echo '\n[sshd]\nport = 9622' >> /etc/fail2ban/jail.local
+echo '' > /etc/motd
+systemctl reload sshd
+systemctl reload fail2ban
